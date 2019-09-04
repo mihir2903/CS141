7fd8a2d5dd9631a00b0cf5a7b916bf906f530e04
mihir2903 <54623570+mihir2903@users.noreply.github.com>
2019-09-04 22:22:28 +0530

    Add files via upload

--- q1.py
+++ q1.py
@@ -1,15 +1,5 @@
-friends= ['Aayush', 'Ruchi', 'Rutwik', 'Utsav']
-print(friends[0])
-print(friends[1])
-print(friends[2])
-print(friends[3])
+message="hello"
+print(message)
 
-print("Hello there, " + friends[0])
-print("Hello there, " + friends[1])
-print("Hello there, " + friends[2])
-print("Hello there, " + friends[3])
-
-cars= ['Audi','Porsche', 'Bugatti']
-print("Owning a "+ cars[0]+ " is a dream come true.")
-print(cars[1]+ " has the most beautiful desgin.")
-print(cars[2]+ " makes the fastest cars.")
+message="bye"
+print(message)
